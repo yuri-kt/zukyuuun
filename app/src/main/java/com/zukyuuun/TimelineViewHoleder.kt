7195fcd6d3a66fdccf1e62a427194087f0be3a52
yogita109 <ush109rock@gmail.com>
2019-06-01 13:11:18 +0900

    add recycle view holder

---
+++ app/src/main/java/com/zukyuuun/TimelineViewHoleder.kt
@@ -0,0 +1,9 @@
+package com.zukyuuun
+
+import android.support.v7.widget.RecyclerView
+import android.view.View
+import com.zukyuuun.databinding.ItemTimelineBinding
+
+class TimelineViewHolder(val binding: ItemTimelineBinding): RecyclerView.ViewHolder(binding.root) {
+
+}
