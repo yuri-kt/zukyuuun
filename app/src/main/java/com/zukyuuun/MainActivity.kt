86e4f47f32a15d8a5b16379ddd7b822203d544c9
yogita109 <ush109rock@gmail.com>
2019-06-01 13:12:28 +0900

    display recycler view item

--- app/src/main/java/com/zukyuuun/MainActivity.kt
+++ app/src/main/java/com/zukyuuun/MainActivity.kt
@@ -1,12 +1,22 @@
 package com.zukyuuun
 
+import android.databinding.DataBindingUtil
+import android.databinding.adapters.LinearLayoutBindingAdapter
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import com.zukyuuun.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        val list = listOf("dummy1", "dummy2", "dummy3")
+        binding.recyclerView.adapter = TimelineAdapter(list)
+        binding.recyclerView.layoutManager = LinearLayoutManager(this)
     }
 }
